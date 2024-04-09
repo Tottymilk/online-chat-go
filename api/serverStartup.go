9cfa942b7fb48714090f69c60197ebc2cf7a85c5
artyomkorchagin <steamartem@gmail.com>
2024-04-09 16:14:21 +0300

    minor restructure

--- api/serverStartup.go
+++ api/serverStartup.go
@@ -8,8 +8,9 @@ import (
 )
 
 func handleRequests() {
+	s := server.Server
 	router := mux.NewRouter()
 	router.HandleFunc("/", mainPage)
 	router.HandleFunc("/sendMessage", sendMessage).Methods("post")
-	log.Fatal(http.ListenAndServe(":8085", router))
+	log.Fatal(http.ListenAndServe(s.listenAdress, router))
 }
