cab8e54ab47b8d7f0c2029909fdf40c3349ab26d
artyomkorchagin <steamartem@gmail.com>
2024-05-05 20:00:31 +0300

    its working
    finally found the solution.
    1. somehow i havent noticed that ive deleted the htmx script line.
    2. turns out i had to use htmx.on() targeting the form and using htmx.afterRequest event listener. using it on the button conflicts with hx-post

--- static/js/websockets.js
+++ static/js/websockets.js
@@ -1,21 +1,36 @@
 let clientWS = new WebSocket("ws://localhost:8085/ws")
 
-window.addEventListener('beforeunload', () => {
-    clientWS.send("somebody just left :(")
-})
-
-var chat_box = document.getElementById("chat-box")
+clientWS.onopen = () => {
+    clientWS.send("yay somebody joined :)")
+}
 
 clientWS.onmessage = (event) => {
     console.log("message recieved: ", event.data)
     chat_box.textContent = chat_box.textContent + event.data
 }
 
+window.addEventListener('beforeunload', () => {
+    clientWS.send("somebody just left :(")
+})
+
+var chat_box = document.getElementById("chat-box")
 var input_field = document.getElementById("user-message")
-var send_msg_btn = document.getElementById("send-message")
 var send_msg_form = document.getElementById("send-message-form")
 
+htmx.on(send_msg_form,"htmx:afterRequest", () => {
+    console.log("message had been sent: ", input_field.value)
+    // TODO: make date time appear from the database datestamp, this is unreliable
+    clientWS.send(" " + getCurrentDateTime() + " | " + input_field.value)
+    input_field.value = ""   
+})
 
-clientWS.onopen = () => {
-    clientWS.send("yay somebody joined :)")
-}
+function getCurrentDateTime() {
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = (now.getMonth() + 1).toString().padStart(2, '0');
+    let day = now.getDate().toString().padStart(2, '0');
+    let hours = now.getHours().toString().padStart(2, '0');
+    let minutes = now.getMinutes().toString().padStart(2, '0');
+    let dateTime = day + "-" + month + "-" + year + " " + hours + ":" + minutes;
+    return dateTime;
+  }
